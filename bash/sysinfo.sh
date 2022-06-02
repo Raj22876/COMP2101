6cd46a7320400cf2d3992514a082f1b77238b02a
Raj Rajput <rajrajput22876@gmail.com>
2022-06-02 13:37:26 -0400

    sending my files

---
+++ bash/sysinfo.sh
@@ -0,0 +1,15 @@
+# System Information 
+
+# Hostname, operating system and architecture 
+hostnamectl
+
+# Domain name
+  domainname -d
+
+# Ip address used by the machine 
+    domainname -I
+    
+# Capacity of root directory 
+df -h /root
+
+exit
